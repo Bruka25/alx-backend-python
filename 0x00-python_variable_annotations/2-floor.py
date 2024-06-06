19ceb084ae2e9c018ebb2533685162e98dd55a37
Bruka25 <bkteshome862@gmail.com>
2024-06-06 21:46:58 +0300

    a function that returns which takes float and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Module for a function which takes a float n as argument and returns
+   the floor of the float
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float number."""
+    return math.floor(n)
