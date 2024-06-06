f43c655e3cad86e78d341d6032c41570c6ba4179
Bruka25 <bkteshome862@gmail.com>
2024-06-06 21:36:08 +0300

    a function that takes two strings and returns thier concatination

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""This module is for function that takes string and
+   returns a concatinated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
+    return str1 + str2
