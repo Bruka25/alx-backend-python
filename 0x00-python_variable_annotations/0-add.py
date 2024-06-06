a4186ca06ddf9d682adaca6f1a78158576396ded
Bruka25 <bkteshome862@gmail.com>
2024-06-06 21:12:48 +0300

    add readme and first task for returning the sum as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""This is the module for the type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns their sum."""
+    return a + b
