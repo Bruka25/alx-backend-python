94d1e5c63548c72e1aa4b9636f2db4a9f2befede
Bruka25 <bkteshome862@gmail.com>
2024-07-01 14:42:20 +0000

    test case for mock HTTP request

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,10 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Dict, Tuple
+from unittest.mock import patch, Mock
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for the access_nested_map function."""
@@ -39,6 +41,38 @@ class TestAccessNestedMap(unittest.TestCase):
         """Function for assesing nested exception raising."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test cases for the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test get_json returns the expected result.
+
+        Parameters:
+        test_url (str): The URL to fetch the JSON from.
+        test_payload (dict): The expected JSON payload.
+        """
+        with patch('utils.requests.get') as mock_get:
+            # Create a Mock response object with the desired json method
+            # return value
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            # Call the function with the test URL
+            result = get_json(test_url)
+
+            # Assert that requests.get was called once with the test URL
+            mock_get.assert_called_once_with(test_url)
+
+            # Assert that the result is equal to the test payload
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
